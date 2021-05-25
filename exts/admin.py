a152dd64998f86a0eac30cbe31f63778d9a51fb0
Sofia "dzshn" N. L <zshn@pm.me>
2021-05-25 19:53:48 -0300

    Improve formatting for eval & update error handler

--- exts/admin.py
+++ exts/admin.py
@@ -1,10 +1,7 @@
-import asyncio
 import contextlib
-import concurrent.futures
 import io
 import textwrap
 import traceback
-import threading
 import time
 
 import discord
@@ -13,23 +10,32 @@ import sys
 
 
 codeblock_wrapper = textwrap.TextWrapper(
-    width=1000, max_lines=32, placeholder='…',
-    initial_indent='```py\n', break_on_hyphens=False,
-    replace_whitespace=False, drop_whitespace=False
+    width=1000, placeholder='…', initial_indent='```py\n',
+    break_on_hyphens=False, replace_whitespace=False,
+    drop_whitespace=False
 )
 
-def format_codeblock(text: str) -> str:
+def format_codeblock(text: str):
+    text = str(text)
     if text == '':
-        return '```<empty>```'
+        return
 
     formatted = codeblock_wrapper.fill(str(text))
-
-    if len(formatted) > 1024:
-        formatted = formatted[:1024] + '…'
+    if len(formatted) > 1000:
+        formatted = formatted[:1000] + '…'
 
     formatted = formatted + '\n```'
 
     return formatted
+
+def get_files(*contents):
+    files = []
+    for text in map(str, contents):
+        if text and len(codeblock_wrapper.fill(text)) > 1000:
+            files.append(discord.File(io.StringIO(text), 'trunc.py'))
+
+    return files
+
 
 class Admin(commands.Cog, command_attrs={"hidden": True}):
     def __init__(self, bot: commands.Bot):
@@ -38,29 +44,7 @@ class Admin(commands.Cog, command_attrs={"hidden": True}):
     def cog_check(self, ctx):
         return self.bot.is_owner(ctx.author)
 
-    @commands.command()
-    async def doc(self, ctx, obj: str):
-        """Returns __doc__ for given object"""
-        try:
-            doc = (__builtins__|globals()|locals())[obj.split('.')[0]]
-
-        except KeyError:
-            await ctx.send(f'Obj {obj} not found!')
-            return
-
-        for i in obj.split('.')[1:]:
-            try:
-                doc = getattr(doc, i)
-
-            except AttributeError:
-                await ctx.send(f'Attribute {i} does not exist!')
-                return
-
-        doc = doc.__doc__
-
-        await ctx.send(file=discord.File(io.StringIO(doc),'a.py'))
-
-    @commands.command()
+    @commands.command(aliases=['.'])
     async def eval(self, ctx, *, code: str):
         if code.startswith('```'):
             code = '\n'.join(code.splitlines()[1:-1])
@@ -70,43 +54,55 @@ class Admin(commands.Cog, command_attrs={"hidden": True}):
 
         code = 'async def func():\n' + textwrap.indent(code, '  ')
         code_return = '<empty>'
-        code_stdout = io.BytesIO()
+        code_stdout = io.StringIO()
         env = {**globals()|locals()}
-
         try:
+            exec_time = time.perf_counter()
             exec(code, env)
             with contextlib.redirect_stdout(code_stdout):
                 code_return = await env['func']()
 
         except Exception as exc:
-            stdout_formatted = format_codeblock(code_stdout.getvalue().decode())
+            return_formatted = format_codeblock(code_return)
+            stdout_formatted = format_codeblock(code_stdout.getvalue())
             traceback_formatted = format_codeblock(traceback.format_exc(-1))
-            return_formatted = textwrap.shorten(str(code_return), width=36, placeholder='…')
+            embed = discord.Embed(
+                color=0xfa5050,
+                title=f":x: {exc!r}!",
+                description=f"{time.perf_counter()-exec_time:3g}s :clock2:"
+            )
+
+            embed.add_field(name='Traceback', value=traceback_formatted, inline=False)
+            embed.add_field(name='Return', value=return_formatted, inline=False)
+            if stdout_formatted:
+                embed.add_field(name='Stdout', value=stdout_formatted, inline=False)
+
             await ctx.send(
-                embed=discord.Embed(
-                    color=0xfa5050,
-                    title=f":x: {exc}!",
-                    description=f"Return → `{return_formatted}` "
-                ).add_field(
-                    name='Stdout', value=stdout_formatted, inline=False
-                ).add_field(
-                    name='Traceback', value=traceback_formatted, inline=False
-                )
+                embed=embed,
+                files=get_files(code_return, code_stdout.getvalue(), traceback.format_exc(-1))
             )
 
         else:
-            stdout_formatted = format_codeblock(code_stdout.getvalue().decode())
-            return_formatted = textwrap.shorten(str(code_return), width=36, placeholder='…')
-            await ctx.send(
-                embed=discord.Embed(
-                    color=0x50fa50,
-                    title=f":white_check_mark: Code evaluated",
-                    description=f"Return → `{return_formatted}` "
-                ).add_field(
-                    name='Stdout', value=stdout_formatted, inline=False
-                )
+            return_formatted = format_codeblock(code_return)
+            stdout_formatted = format_codeblock(code_stdout.getvalue())
+            embed = discord.Embed(
+                color=0x50fa50,
+                title=f":white_check_mark: Code evaluated",
+                description=f"{time.perf_counter()-exec_time:g}s :clock2:"
+            )
+            embed.add_field(
+                name='Return', value=return_formatted, inline=False
             )
 
+            if stdout_formatted:
+                embed.add_field(
+                    name='Stdout', value=stdout_formatted, inline=False
+                )
+
+            await ctx.send(
+                embed=embed,
+                files=get_files(code_return, code_stdout.getvalue())
+            )
 
 
 def setup(bot):
