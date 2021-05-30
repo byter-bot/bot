86581dc0d36e79005883a25e5c600c245f6d1cfd
Sofia "dzshn" N. L <zshn@pm.me>
2021-05-30 17:55:32 -0300

    Add textimg command Use aiohttp.ClientSession as context manager instead · Session would sometimes close randomly (?) · Script could finish with multiple warnings for unclosed sessions if extension is reloaded

--- exts/fun.py
+++ exts/fun.py
@@ -6,24 +6,24 @@ from discord.ext import commands
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.session = aiohttp.ClientSession(loop=bot.loop)
 
     @commands.command()
     async def cat(self, ctx):
         """cat"""
-        resp = await self.session.get("https://api.thecatapi.com/v1/images/search")
-        if resp.ok:
-            json = await resp.json()
-            url = json[0]['url']
-            await ctx.send(
-                embed=discord.Embed(
-                    color=0x5050fa,
-                    title='Cat!!'
-                ).set_image(url=url)
-            )
+        async with aiohttp.ClientSession(loop=self.bot.loop) as session:
+            resp = await session.get("https://api.thecatapi.com/v1/images/search")
+            if resp.ok:
+                json = await resp.json()
+                url = json[0]['url']
+                await ctx.send(
+                    embed=discord.Embed(
+                        color=0x5050fa,
+                        title='Cat!!'
+                    ).set_image(url=url)
+                )
 
-        else:
-            await ctx.send("Couldn't get a cat image <:creustickersad:726924232461910077>")
+            else:
+                await ctx.send("Couldn't get a cat image <:creustickersad:726924232461910077>")
 
 
 def setup(bot):
