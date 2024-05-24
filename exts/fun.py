ad85679de1035b6f9e2065d24ba0a9859f889788
rini <rini@rinici.de>
2024-05-24 07:07:56 -0300

    Add status system Add automatic reactions Add command for reloading config

--- exts/fun.py
+++ exts/fun.py
@@ -7,6 +7,24 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.guild.id == self.bot.config['main_guild_id']:
+            return
+
+        filtered_msg = ''.join(
+            char for char in message.content.lower() if 'a' <= char <= 'z' or char == ' '
+        )
+
+        for reaction, triggers in self.bot.config['reactions'].items():
+            for trigger in triggers:
+                if trigger in filtered_msg.split(' '):
+                    if reaction.isdecimal:
+                        reaction = self.bot.get_emoji(int(reaction))
+
+                    await message.add_reaction(reaction)
+                    break
+
     @commands.command()
     async def cat(self, ctx):
         """cat"""
