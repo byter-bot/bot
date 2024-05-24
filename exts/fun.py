2359759f3538fd78b81d946b9519b3a1047f9863
rini <rini@rinici.de>
2024-05-24 07:07:54 -0300

    cat

---
+++ exts/fun.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def cat(self, ctx):
+        """cat"""
+        resp = await self.bot.session.get("https://api.thecatapi.com/v1/images/search")
+        if resp.ok:
+            json = await resp.json()
+            url = json[0]['url']
+            await ctx.send(
+                embed=discord.Embed(
+                    color=0x5050fa,
+                    title='Cat!!'
+                ).set_image(url=url)
+            )
+
+        else:
+            await ctx.send("Couldn't get a cat image <:creustickersad:726924232461910077>")
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
