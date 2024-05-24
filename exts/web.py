c3952e3d0e056457ae79e098c88342b91b0b1867
rini <rini@rinici.de>
2024-05-24 07:07:55 -0300

    Add wikipedia command Limit error name in unknown error to 80 chars · aiohttp sometimes made huge errors when debugging and reach the embed limit

---
+++ exts/web.py
@@ -0,0 +1,62 @@
+import aiohttp
+import html
+import io
+import json
+import re
+import typing
+import urllib.parse
+
+import discord
+from discord.ext import commands
+
+
+class Web(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['wiki'])
+    async def wikipedia(self, ctx, limit: typing.Optional[int]=4, *, query: urllib.parse.quote):
+        """Searches wikipedia
+
+        You may optionally provide a limit for the results before the query (defaults to 4)"""
+        if not 1 <= limit <= 25:
+            raise commands.BadArgument('search limit must be between 1 and 25')
+
+        async with aiohttp.ClientSession(loop=self.bot.loop) as session:
+            params = {
+                'action': 'query',
+                'format': 'json',
+                'list': 'search',
+                'srprop': 'snippet',
+                'srsearch': query,
+                'srlimit': limit
+            }
+            async with session.get('https://en.wikipedia.org/w/api.php', params=params) as resp:
+                if not resp.ok:
+                    await ctx.send('An error occured querying data')
+                    return
+
+                data = (await resp.json())['query']['search']
+                embed = discord.Embed(
+                    color=0x5050fa,
+                    title=f'Wikipedia results for "{query}"'
+                )
+
+                for index, obj in enumerate(data):
+                    link = f'… [link](https://wikipedia.org/wiki/{urllib.parse.quote(obj["title"])})'
+                    embed.add_field(
+                        name=f'{index+1}: {obj["title"]}',
+                        value=html.unescape(re.sub(r'<.*?>', '', obj['snippet'])),
+                        inline=False
+                    )
+
+                if limit < 10:
+                    await ctx.send(embed=embed)
+
+                else:
+                    await ctx.author.send(embed=embed)
+                    await ctx.message.add_reaction('\N{white heavy check mark}')
+
+
+def setup(bot):
+    bot.add_cog(Web(bot))
